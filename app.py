845f0ae11706552479fe9701a5c0a9b914a2ad69
pradeepkundekar0101 <pradeepkundekargithub@gmail.com>
2024-07-16 16:23:09 +0530

    Added Get Trade Book API

--- app.py
+++ app.py
@@ -1,36 +1,51 @@
-
 from flask import Flask, request, jsonify
 from dhanhq import dhanhq
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Server is working'
 
-@app.route('/holdings')
+@app.route("/")
+def index():
+    return "Server is working"
+
+
+@app.route("/holdings")
 def get_holdings():
     try:
-        client_id = request.args.get('clientId', '')
-        access_token = request.args.get('accessToken', '')
-        
+        client_id = request.args.get("clientId", "")
+        access_token = request.args.get("accessToken", "")
+
         dhan = dhanhq(client_id, access_token)
         holding_data = dhan.get_holdings()
         return jsonify(holding_data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/positions')
+
+@app.route("/positions")
 def get_positions():
     try:
-        client_id = request.args.get('clientId', '')
-        access_token = request.args.get('accessToken', '')
-        
+        client_id = request.args.get("clientId", "")
+        access_token = request.args.get("accessToken", "")
+
         dhan = dhanhq(client_id, access_token)
         position_data = dhan.get_positions()
         return jsonify(position_data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
+
+@app.route("/get_trade_book")
+def get_trade_book():
+    try:
+        client_id = request.args.get("clientId", "")
+        access_token = request.args.get("accessToken", "")
+        dhan = dhanhq(client_id, access_token)
+        position_data = dhan.get_trade_book()
+        return jsonify(position_data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
     app.run(debug=True)
