3ebd99677556adffcde6988f3c9b2dda58214c32
pradeepkundekar0101 <pradeepkundekargithub@gmail.com>
2024-09-02 20:32:26 +0530

    added procfile

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from dhanhq import dhanhq
 
@@ -59,4 +60,5 @@ def get_funds():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=True)
